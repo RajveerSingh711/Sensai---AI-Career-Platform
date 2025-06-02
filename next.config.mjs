8732a6fe116d2e3e35a0a3e8923934523b11226b
RajveerSingh711 <rajveer032020@gmail.com>
2025-06-02 19:42:14 +0530

    issue fixing

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        pathname: "/**",
-      },
-    ],
+    domains: ['randomuser.me'],
   },
 };
 
